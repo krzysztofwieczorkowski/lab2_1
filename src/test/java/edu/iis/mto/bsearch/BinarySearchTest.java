a60e5a2dd52aabe11b64a09f7fc7a3299d2d02e3
krzysztofwieczorkowski <wieczorkowski.k7@gmail.com>
2021-03-15 19:09:38 +0100

    testy_v2

--- src/test/java/edu/iis/mto/bsearch/BinarySearchTest.java
+++ src/test/java/edu/iis/mto/bsearch/BinarySearchTest.java
@@ -12,41 +12,40 @@ class BinarySearchTest {
     void setUp() throws Exception {}
 
     @Test
-    void test1() {
+    public void ifElementExists_oneElementArray_true () {
         int[] array={5};
         SearchResult res = BinarySearch.search(5,array);
         assertEquals(res.isFound(),true);
     }
     @Test
-    void test2() {
+    public void ifElementExists_oneElementArray_false() {
         int[] array={15};
         SearchResult res = BinarySearch.search(5,array);
         assertEquals(res.isFound(),false);
     }
     @Test
-    void test3() {
+    public void searchForFirstElement_manyElementArray() {
         int[] array={2,4,6,8,10,12};
         SearchResult res = BinarySearch.search(2,array);
         assertEquals(res.getPosition(),1);
     }
     @Test
-    void test4() {
+    public void searchForLastElement_manyElementArray() {
         int[] array={3,6,9,12,15,18};
         SearchResult res = BinarySearch.search(18,array);
         assertEquals(res.getPosition(),6);
     }
     @Test
-    void test5() {
+    public void searchForMiddleElement_manyElementArray() {
         int[] array={4,8,12,16,20};
         SearchResult res = BinarySearch.search(12,array);
         assertEquals(res.getPosition(),3);
     }
     @Test
-    void test6() {
+    public void searchForElement_noSuchElement() {
         int[] array={3,5,18,24,85,148};
         SearchResult res = BinarySearch.search(2,array);
         assertEquals(res.isFound(),false);
     }
-
 
 }
